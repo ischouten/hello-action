7d96e264aa518b58f50b3ccb1e44724bda94e90f
Igor Schouten <igors@connecterra.io>
2021-03-13 16:20:45 +0100

    Add linter and set it up in workflow

--- HelloHttp/__init__.py
+++ HelloHttp/__init__.py
@@ -1,9 +1,9 @@
 import logging
-
+import typing
 import azure.functions as func
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info("Python HTTP trigger function processed a request. This line is also going to be way too long.")
 
-    return func.HttpResponse(f"Hello! This worked!", status_code=200)
+    return func.HttpResponse("Hello! This worked!", status_code=200)
